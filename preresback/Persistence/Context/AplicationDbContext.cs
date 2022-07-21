25a4acf5e8131ba7f45afc6561327bf4f2ba2664
Gerardo Alvarez Mendoza <gerardam@outlook.com>
2022-07-21 13:17:12 -0500

    Add project files.

---
+++ preresback/Persistence/Context/AplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using preresback.Domain.Models;
+
+namespace preresback.Persistence.Context
+{
+    public class AplicationDbContext : DbContext
+    {
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
